72873c44a0c4a6e1a596a85c52c3157ac88a27a8
Michelle7220 <michellehuang722@gmail.com>
2022-08-22 15:21:39 -0500

    first commit frontend

---
+++ src/authPages/LoginPage/LoginPageinputs.js
@@ -0,0 +1,31 @@
+import React from "react";
+import InputWithLabel from "../../shared/components/InputWithLabel";
+
+
+const LoginPageinputs = ({mail,setMail, password, setPassword}) => {
+    return (
+    <>
+    <InputWithLabel
+    value={mail}
+    setValue={setMail}
+    label="E-mail"
+    type="text"
+    placeholder="Enter e-mail address"
+    />
+
+<InputWithLabel
+    value={password}
+    setValue={setPassword}
+    label="Password"
+    type="password"
+    placeholder="Enter password"
+    />
+
+
+
+
+    </>
+    );
+};
+
+export default LoginPageinputs;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
 import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from  "./store/store";
+import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+<React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
